9573afef1cbd8e3aed56870fdaa34ad9955e17a2
Foxety <rsslovo@yandx.ru>
2018-09-29 20:31:15 +0300

    Init spida

---
+++ src/main/java/spidaApp/SpidaApplication.java
@@ -0,0 +1,12 @@
+package spidaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpidaApplication.class, args);
+	}
+}
