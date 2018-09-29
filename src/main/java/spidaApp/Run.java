5a372cbb70b31eac54720074b973e6ff01e550cd
Foxety <rsslovo@yandx.ru>
2018-09-29 22:21:50 +0300

    Normal start Server with Security

--- src/main/java/spidaApp/Run.java
+++ src/main/java/spidaApp/Run.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpidaApplication {
+public class Run {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpidaApplication.class, args);
+		SpringApplication.run(Run.class, args);
 	}
 }
